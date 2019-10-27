02b80be2f5e46159e4f0fedf77caa50ed724000b
happyzhang <55052017+happyzhang1995@users.noreply.github.com>
2019-10-27 23:55:22 +0800

    Update AdGuardHome.lua

--- luasrc/model/cbi/AdGuardHome.lua
+++ luasrc/model/cbi/AdGuardHome.lua
@@ -1,6 +1,6 @@
 local fs=require"nixio.fs"
 mp = Map("AdGuardHome", translate("AdGuard Home"))
-mp.description = translate("免费和开源，功能强大的全网络广告和跟踪程序阻止DNS服务器")
+mp.description = translate("免费和开源，功能强大的全网络广告和跟踪程序拦截DNS服务器")
 
 mp:section(SimpleSection).template  = "AdGuardHome/AdGuardHome_status"
 
@@ -11,7 +11,13 @@ s.addremove=false
 enabled = s:option(Flag, "enabled", translate("启用广告屏蔽"))
 enabled.default = 0
 enabled.rmempty = false
-enabled.description = translate("启用后，AdGuardHome默认管理IP为：<font color=\"blue\"><strong>路由ip:3000</strong></font> （例：192.168.123.1:3000）")
+
+enabled =s:option(Value,"port",translate("管理端口"))
+enabled.placeholder=3000
+enabled.default=3000
+enabled.datatype="port"
+enabled.rmempty=false
+enabled.description = translate("<input type=\"button\" value=\" "..translate("AdGuardHome管理界面").." \" onclick=\"window.open('http://'+window.location.hostname+':"..port.."/')\"/>注；管理端口无法真正设置AdGuardHome管理端口，只是提供一个网页快捷方式")
 
 local apply = luci.http.formvalue("cbi.apply")
  if apply then
